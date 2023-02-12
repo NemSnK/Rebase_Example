205108ef42bc5015180ecd01b963f969b6d9b287
user <user@24-003361.iteco-inno.ru>
2023-02-12 16:51:49 +0300

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+
+public class Main {
+    public static void main(String[] args) {
+        Person sara = new Person("Sara", 4);
+        Person viktor = new Person("Viktor", 40);
+        Person eva = new Person("Eva", 42);
+        Person anna = new Person("Anna", 5);
+        List<Person> collection = asList(sara, eva, viktor, anna);
+
+        System.out.println(getKidNames(collection));
+    }
+
+    public static Set<String> getKidNames(List<Person> people) {
+        return people.stream()
+                .filter(person -> person.getAge() <18)
+                .map(Person::getName)
+                .collect(Collectors.toSet());
+    }
+}
